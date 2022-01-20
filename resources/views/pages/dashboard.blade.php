e11b99e3745f696ec1df6d9993087808b4a518cd
rizkaulia19 <52026584+rizkaulia19@users.noreply.github.com>
2022-01-20 19:05:46 +0700

    chart dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -92,13 +92,13 @@
                 </div>
             </div>
         </div>
-        <div class="card mb-4">
+        <!-- <div class="card mb-4">
             <div class="card-header">
                 <i class="fas fa-chart-area me-1"></i>
                 Total User yang mendaftar
             </div>
             <div class="card-body"><canvas id="myAreaChart" width="100%" height="30"></canvas></div>
-        </div>
+        </div> -->
         <div class="row">
             <div class="col-lg-6">
                 <div class="card mb-4">
@@ -298,55 +298,55 @@ var pie_chart_keahlian_tersedia = new Chart(ctx, {
 });
 
 //Chart area Total user mendaftar
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["Mar 10", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
-        datasets: [{
-            label: "Sessions",
-            lineTension: 0.3,
-            backgroundColor: "rgba(2,117,216,0.2)",
-            borderColor: "rgba(2,117,216,1)",
-            pointRadius: 5,
-            pointBackgroundColor: "rgba(2,117,216,1)",
-            pointBorderColor: "rgba(255,255,255,0.8)",
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(2,117,216,1)",
-            pointHitRadius: 50,
-            pointBorderWidth: 2,
-            data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
-        }],
-    },
-    options: {
-        scales: {
-            xAxes: [{
-                time: {
-                    unit: 'date'
-                },
-                gridLines: {
-                    display: false
-                },
-                ticks: {
-                    maxTicksLimit: 7
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    min: 0,
-                    max: 40000,
-                    maxTicksLimit: 5
-                },
-                gridLines: {
-                    color: "rgba(0, 0, 0, .125)",
-                }
-            }],
-        },
-        legend: {
-            display: false
-        }
-    }
-});
+// var ctx = document.getElementById("myAreaChart");
+// var myLineChart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: ["Mar 10", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+//         datasets: [{
+//             label: "Sessions",
+//             lineTension: 0.3,
+//             backgroundColor: "rgba(2,117,216,0.2)",
+//             borderColor: "rgba(2,117,216,1)",
+//             pointRadius: 5,
+//             pointBackgroundColor: "rgba(2,117,216,1)",
+//             pointBorderColor: "rgba(255,255,255,0.8)",
+//             pointHoverRadius: 5,
+//             pointHoverBackgroundColor: "rgba(2,117,216,1)",
+//             pointHitRadius: 50,
+//             pointBorderWidth: 2,
+//             data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+//         }],
+//     },
+//     options: {
+//         scales: {
+//             xAxes: [{
+//                 time: {
+//                     unit: 'date'
+//                 },
+//                 gridLines: {
+//                     display: false
+//                 },
+//                 ticks: {
+//                     maxTicksLimit: 7
+//                 }
+//             }],
+//             yAxes: [{
+//                 ticks: {
+//                     min: 0,
+//                     max: 40000,
+//                     maxTicksLimit: 5
+//                 },
+//                 gridLines: {
+//                     color: "rgba(0, 0, 0, .125)",
+//                 }
+//             }],
+//         },
+//         legend: {
+//             display: false
+//         }
+//     }
+// });
 
 //Chart line total transaksi
 var ctx = document.getElementById("transactionSumChart");
