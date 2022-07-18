2ad69b0b4af196c2ccc00f38b16f091505456d88
rizkaulia19 <52026584+rizkaulia19@users.noreply.github.com>
2022-07-18 14:41:40 +0700

    proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,12 +13,12 @@ class TrustProxies extends Middleware
      * @var array|string
      */
     protected $proxies = '*';
-    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
+    
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
 }
