a5d1e14cf82a8cb5ffef4bc072c01e290679a611
rizkaulia19 <52026584+rizkaulia19@users.noreply.github.com>
2022-07-18 14:40:09 +0700

    logging

--- config/logging.php
+++ config/logging.php
@@ -38,12 +38,13 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
+            // 'ignore_exceptions' => false,
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            // 'driver' => 'single',
+            'driver' => 'errorlog',
+            // 'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
 
