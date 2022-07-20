f6cb2bd6efd2a07c434fc0fff8c31fe074ee9edf
zhuan.akbar <zhuan.akbar@salt.co.id>
2022-07-20 20:59:25 +0700

    change criteria constant to calculate logic

--- app/Repository/CriteriaRepository.php
+++ app/Repository/CriteriaRepository.php
@@ -14,12 +14,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class CriteriaRepository extends BaseRepository
 {
-    private const C1_WEIGHT = -0.125;
-    private const C2_WEIGHT = 0.2;
-    private const C3_WEIGHT = -0.175;
-    private const C4_WEIGHT = 0.3;
-    private const C5_WEIGHT = 0.125;
-    private const C6_WEIGHT = -0.075;
+    private $c1_Normalize;
+    private $c2_Normalize;
+    private $c3_Normalize;
+    private $c4_Normalize;
+    private $c5_Normalize;
+    private $c6_Normalize;
+
+    public function __construct()
+    {
+        $this->normalizeCriteria();
+    }
 
     public function getCriteria(): array
     {
@@ -56,10 +61,16 @@ class CriteriaRepository extends BaseRepository
             $filerByStatus = $filters['status_project'] ??= 'succeed';
             $query->select(['specializations.*', 'project_app.id as appId', 'projects.id as projectId']);
             $query->leftJoin(
-                'project_specializations as projects', 'projects.specializationId','=', 'specializations.id'
+                'project_specializations as projects',
+                'projects.specializationId',
+                '=',
+                'specializations.id'
             );
             $query->leftJoin(
-                'project_applicants as project_app', 'project_app.projectId', '=', 'projects.projectId'
+                'project_applicants as project_app',
+                'project_app.projectId',
+                '=',
+                'projects.projectId'
             );
             $query->where('specializations.id', $specializationsId);
             $query->where('project_app.status', $filerByStatus);
@@ -70,7 +81,11 @@ class CriteriaRepository extends BaseRepository
     public function getRevenueSpecialization(string $specializationsId = ''): int
     {
         $query = $this->getSpecializations(
-            new ProjectSpecialization, $specializationsId, 'projectId', new Project, 'id'
+            new ProjectSpecialization,
+            $specializationsId,
+            'projectId',
+            new Project,
+            'id'
         );
         return $query->sum('cost');
     }
@@ -78,54 +93,84 @@ class CriteriaRepository extends BaseRepository
     public function findC1(string $specializationIds = ''): float
     {
         $query = $this->getSpecializations(
-            new UserSpecialization, $specializationIds, 'userId', new ProjectApplicant, 'userId'
+            new UserSpecialization,
+            $specializationIds,
+            'userId',
+            new ProjectApplicant,
+            'userId'
         );
-        return $query->count() === 0 ? 0.0 : pow($query->count(), self::C1_WEIGHT);
+        return $query->count() === 0 ? 0.0 : pow($query->count(), $this->c1_Normalize);
     }
 
     public function findC2(string $specializationIds = ''): float
     {
         $query = $this->getSpecializations(
-            new ProjectSpecialization, $specializationIds, 'projectId', new Project, 'id'
+            new ProjectSpecialization,
+            $specializationIds,
+            'projectId',
+            new Project,
+            'id'
         );
         $revenue = $query->sum('cost');
-        return pow($revenue, self::C2_WEIGHT);
+        return pow($revenue, $this->c2_Normalize);
     }
 
     public function findC3(string $specializationIds = ''): float
     {
         $query = UserSpecialization::where('specializationId', $specializationIds);
-        return $query->count() === 0 ? 0.0 :  pow($query->count(), self::C3_WEIGHT);
+        return $query->count() === 0 ? 0.0 :  pow($query->count(), $this->c3_Normalize);
     }
 
     public function findC4(string $specializationIds = ''): float
     {
         $query = ProjectSpecialization::where('specializationId', $specializationIds);
-        return $query->count() === 0 ? 0.0 :  pow($query->count(), self::C4_WEIGHT);
+        return $query->count() === 0 ? 0.0 :  pow($query->count(), $this->c4_Normalize);
     }
 
     public function findC5(string $specializationIds = ''): float
     {
         $query = $this->getSpecializations(
-            new ProjectSpecialization, $specializationIds, 'projectId', new ProjectApplicant, 'projectId'
+            new ProjectSpecialization,
+            $specializationIds,
+            'projectId',
+            new ProjectApplicant,
+            'projectId'
         );
         $query->where('status', 'succeed');
-        return  $query->count() === 0 ? 0.0 :  pow($query->count(), self::C5_WEIGHT);
+        return  $query->count() === 0 ? 0.0 :  pow($query->count(), $this->c5_Normalize);
     }
 
     public function findC6(string $specializationIds = ''): float
     {
         $query = ClickSpecialization::where('specializationId', $specializationIds);
-        return   $query->count() === 0 ? 0.0 :  pow($query->count(), self::C6_WEIGHT);
+        return   $query->count() === 0 ? 0.0 :  pow($query->count(), $this->c6_Normalize);
     }
 
     private function getSpecializations(
-        Model $specialModel, string $specializationIds = '', string $arrayColumn,
-        Model $masterModel, string $filterIdColumn
-    ): Builder
-    {
+        Model $specialModel,
+        string $specializationIds = '',
+        string $arrayColumn,
+        Model $masterModel,
+        string $filterIdColumn
+    ): Builder {
         $specializations = $specialModel::where('specializationId', $specializationIds)->get();
         $filteredIds = array_column($specializations->toArray(), $arrayColumn);
         return $masterModel::whereIn($filterIdColumn, $filteredIds);
+    }
+
+    private function normalizeCriteria()
+    {
+        $c1 = 12.5;
+        $c2 = 20;
+        $c3 = 17.5;
+        $c4 = 30;
+        $c5 = 12.5;
+        $c6 = 7.5;
+        $this->c1_Normalize = - ($c1 / ($c1 + $c2 + $c3 + $c4 + $c5 + $c6));
+        $this->c2_Normalize = $c2 / ($c1 + $c2 + $c3 + $c4 + $c5 + $c6);
+        $this->c3_Normalize = - ($c3 / ($c1 + $c2 + $c3 + $c4 + $c5 + $c6));
+        $this->c4_Normalize = $c4 / ($c1 + $c2 + $c3 + $c4 + $c5 + $c6);
+        $this->c5_Normalize = $c5 / ($c1 + $c2 + $c3 + $c4 + $c5 + $c6);
+        $this->c6_Normalize = - ($c6 / ($c1 + $c2 + $c3 + $c4 + $c5 + $c6));
     }
 }
